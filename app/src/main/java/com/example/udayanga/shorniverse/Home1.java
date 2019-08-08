3bb50672fdfb5b77ca778c1975d03983cc675b7e
panda0223 <udayangamaxab29@gmail.com>
2019-08-08 11:10:28 +0530

    this is update

--- app/src/main/java/com/example/udayanga/shorniverse/Home1.java
+++ app/src/main/java/com/example/udayanga/shorniverse/Home1.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Home1 extends AppCompatActivity {
 
-    Button b1,b5;
+    ImageView i1, i2, i3, i4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +17,14 @@ public class Home1 extends AppCompatActivity {
 
 
 
-        b1 = (Button)findViewById(R.id.Account);
-        b5 = (Button)findViewById(R.id.Cart);
+        i1 = (ImageView) findViewById(R.id.buyHome);
+        i2 = (ImageView)findViewById(R.id.sellHome);
+        i3 = (ImageView)findViewById(R.id.cartHome);
+        i4 = (ImageView)findViewById(R.id.accHome);
 
 
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        i1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent q = new Intent(Home1.this, account.class);
@@ -30,13 +33,31 @@ public class Home1 extends AppCompatActivity {
         });
 
 
-        b5.setOnClickListener(new View.OnClickListener() {
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent w = new Intent(Home1.this, SellActiv.class);
+                startActivity(w);
+            }
+        });
+
+        i3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent w = new Intent(Home1.this, cart1.class);
                 startActivity(w);
             }
         });
+
+        i4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent w = new Intent(Home1.this, account.class);
+                startActivity(w);
+            }
+        });
+
+
 
     }
 }
